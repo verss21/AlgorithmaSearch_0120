f5da80ef33331adbc1607e9cfce193e15c6b4b1f
verss21 <arjunafrizal01@gmail.com>
2024-04-05 09:54:45 +0700

    input kedua

--- AlgoritmaSearch/AlgoritmaSearch.cpp
+++ AlgoritmaSearch/AlgoritmaSearch.cpp
@@ -5,11 +5,22 @@ int arr[20]; //array to be searched
 int n; // number of elements in the array
 int i; // index of array element
 
-void input() 
+void input()
 {
 	cout << "Enter the number of elements in the array : ";
 	cin >> n;
 	if ((n > 0) && (n <= 20))
 		break;
 	else
+		cout << "\nArray should have minimum 1 and maximum 20 elements. \n\n";
+}
+
+//accept array elements
+cout << "\n-----------------\n";
+cout << " Enter array elements \n";
+cout << "--------------------\n";
+for (1 = 0; i < n; i++)
+{
+	cout << "<" << (i + 1) << "> ";
+	cin >> arr[i];
 }
