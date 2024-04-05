01e80afbd4ec271e0fe226a544f239124f1c4aa9
verss21 <arjunafrizal01@gmail.com>
2024-04-05 09:48:39 +0700

    input pertama

--- AlgoritmaSearch/AlgoritmaSearch.cpp
+++ AlgoritmaSearch/AlgoritmaSearch.cpp
@@ -5,4 +5,11 @@ int arr[20]; //array to be searched
 int n; // number of elements in the array
 int i; // index of array element
 
-void input()
+void input() 
+{
+	cout << "Enter the number of elements in the array : ";
+	cin >> n;
+	if ((n > 0) && (n <= 20))
+		break;
+	else
+}
