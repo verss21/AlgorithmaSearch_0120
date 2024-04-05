1946b85d9df2e7972ccb7f75333ea289cfd3f82e
verss21 <arjunafrizal01@gmail.com>
2024-04-05 10:02:34 +0700

    penyelesaian

--- AlgoritmaSearch/AlgoritmaSearch.cpp
+++ AlgoritmaSearch/AlgoritmaSearch.cpp
@@ -50,3 +50,8 @@ void LinearSearch()
 			i++;//step 4
 		}
 		if (i == n)// step 5
+			cout << "\n" << item << "not found in the array\n";
+		cout << "nNumber of comparison: " << ctr << endl;
+
+		cout << "\nContinue Search (y/n) : ";
+		cin >> ch;
